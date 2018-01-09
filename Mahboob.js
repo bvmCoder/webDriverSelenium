bebc98c06cef505140411e1a0ae3e42f8c812e98
Dixit Patel <talkwithdixit@gmail.com>
2018-01-09 13:57:01 -0500

    Removed linting configuration

--- Mahboob.js
+++ Mahboob.js
@@ -27,12 +27,12 @@
 	}
 	function reducer(state = count, action) {
 		switch (action) {
-			case 'INC': 
+			case 'INC':
 				return {
 					...state,
 					counter : state.counter + 1
 				}
-			case 'DEC': 
+			case 'DEC':
 				return {
 					...state,
 					counter : state.counter - 1
@@ -47,45 +47,3 @@
 	}
 
 </script>
-const _ = require("lodash");
-
-const bestPracticeRules = require("./rules/best-practices");
-const errorRules = require("./rules/errors");
-const es6Rules = require("./rules/es6");
-const filenameRules = require("./rules/filenames");
-const nodeRules = require("./rules/node");
-const reactRules = require("./rules/react");
-const strictRules = require("./rules/strict");
-const styleRules = require("./rules/style");
-const variableRules = require("./rules/variables");
-
-const allRules = _.assign(bestPracticeRules,
-    errorRules,
-    es6Rules,
-    filenameRules,
-    reactRules,
-    strictRules,
-    styleRules,
-    variableRules,
-    nodeRules);
-
-module.exports = {
-    "env": {
-        "browser": true,
-        "node": true,
-        "es6": true
-    },
-    "ecmaFeatures": {
-        "modules": true,
-        "jsx": true
-    },
-    "parser": "babel-eslint",
-    "plugins": [
-        "filenames",
-        "react"
-    ],
-    "globals": {
-        "__SERVER_RENDERING__": false
-    },
-    "rules": allRules
-};
